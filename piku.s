e6e36234f77e06ab036c34e1a705bfa215c1a036
imanewman <tjnewman111@gmail.com>
2018-04-05 14:14:05 -0700

    Add files via upload

---
+++ piku.s
@@ -0,0 +1,34 @@
+.syntax unified
+
+    @ --------------------------------
+    .global main
+main:
+    @ Stack the return address (lr) in addition to a dummy register (ip) to
+    @ keep the stack 8-byte aligned.
+    push    {ip, lr}
+
+    @ Load the argument and perform the call. This is like 'printf("...")' in C.
+    ldr     r0, =message1
+    bl      printf
+
+    ldr     r0, =message2
+    bl      printf
+
+    ldr     r0, =message3
+    bl      printf
+
+    @ Exit from 'main'. This is like 'return 0' in C.
+    mov     r0, #0      @ Return 0.
+    @ Pop the dummy ip to reverse our alignment fix, and pop the original lr
+    @ value directly into pc — the Program Counter — to return.
+    pop     {ip, pc}
+
+    @ --------------------------------
+    @ Data for the printf call. The GNU assembler's ".asciz" directive
+    @ automatically adds a NULL character termination.
+message1:
+    .asciz  "Hello Raspberry\n"
+message2:
+    .asciz  "That i will be messing with\n"
+message3:
+    .asciz  "I dont like Haikus\n"
